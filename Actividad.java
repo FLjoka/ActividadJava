f339cc3725157ac5f8a6dd9c24ff1407970001cd
Franco <francoljoka2@gmail.com>
2021-11-16 13:15:02 -0300

    modificacion presentacion del ejercicio

--- Actividad.java
+++ Actividad.java
@@ -4,18 +4,26 @@ import javax.swing.JOptionPane;
 
 public class Actividad {
 	public static void main(String[] args) {
+		
 		String nombre = JOptionPane.showInputDialog("Ingrese su nombre");
-		System.out.println(nombre);
 		String apellido = JOptionPane.showInputDialog("Ingrese su apellido");
-		System.out.println(apellido);
 		String edad = JOptionPane.showInputDialog("Ingrese su edad");
-		System.out.println(edad);
 		String hobbie = JOptionPane.showInputDialog("Ingrese un hobbie");
-		System.out.println(hobbie);
 		String editorCodigo = JOptionPane.showInputDialog("Ingrese su editor de codigo preferido");
-		System.out.println(editorCodigo);
 		String sistemaOperativo = JOptionPane.showInputDialog("Ingrese sistema operativo que utiliza");
-		System.out.println(sistemaOperativo);
+		
+		
+		System.out.println("*************************************");
+		System.out.println("******* Bienvenido al sistema *******");
+		System.out.println("*************************************");
+		System.out.println("Los datos ingresados son:");
+		
+		System.out.println("#"+nombre);
+		System.out.println("#"+apellido);
+		System.out.println("#"+edad);
+		System.out.println("#"+hobbie);
+		System.out.println("#"+editorCodigo);
+		System.out.println("#"+sistemaOperativo);
 	}
   
 }
